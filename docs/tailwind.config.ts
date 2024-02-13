c924c4f13f9187761c6dff951e91bab8be242ad0
Pavel Sušický <me@frontendista.cz>
2024-02-13 13:59:41 +0100

    feat(docs): add intro

--- docs/tailwind.config.ts
+++ docs/tailwind.config.ts
@@ -5,7 +5,23 @@ import type { Config } from "tailwindcss";
 const config: Config = {
 	content: ["./src/**/*.{astro,md,mdx,ts,tsx}"],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				sl: {
+					white: "var(--sl-color-white)",
+					"gray-1": "var(--sl-color-gray-1)",
+					"gray-2": "var(--sl-color-gray-2)",
+					"gray-3": "var(--sl-color-gray-3)",
+					"gray-4": "var(--sl-color-gray-4)",
+					"gray-5": "var(--sl-color-gray-5)",
+					"gray-6": "var(--sl-color-gray-6)",
+					black: "var(--sl-color-black)",
+					"accent-low": "var(--sl-color-accent-low)",
+					accent: "var(--sl-color-accent)",
+					"accent-high": "var(--sl-color-accent-high)"
+				}
+			}
+		}
 	},
 	plugins: [starlightPlugin()]
 };
