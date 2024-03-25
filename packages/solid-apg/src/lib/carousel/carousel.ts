731e393756621c61a0fe300c1f3605ef81072248
Pavel Sušický <me@frontendista.cz>
2024-03-25 14:14:14 +0100

    docs(typedoc): add carousel description

--- packages/solid-apg/src/lib/carousel/carousel.ts
+++ packages/solid-apg/src/lib/carousel/carousel.ts
@@ -1,5 +1,7 @@
 /**
- * @link TODO
+ * Creates a Carousel bindings according to the WAI-ARIA specification.
+ *
+ * @link https://www.w3.org/WAI/ARIA/apg/patterns/carousel
  */
 export function createCarousel() {
 	return [];
