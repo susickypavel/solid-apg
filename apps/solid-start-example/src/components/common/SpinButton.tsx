6f678a2e23460f97a13b369d68ff2be6b600cb6b
Pavel Sušický <me@frontendista.cz>
2024-03-26 09:48:54 +0100

    feat(SpinButton): add keyboard controls

--- apps/solid-start-example/src/components/common/SpinButton.tsx
+++ apps/solid-start-example/src/components/common/SpinButton.tsx
@@ -1,22 +1,19 @@
 "use client";
 
-import { JSX, createEffect } from "solid-js";
+import { JSX } from "solid-js";
 import { createSpinbutton } from "../../../../../packages/solid-apg/src";
 
 interface SpinButtonProps extends JSX.InputHTMLAttributes<HTMLInputElement> {}
 
 export function SpinButton(x: SpinButtonProps) {
-	const [{ up, down }, props] = createSpinbutton({
+	const [{ up, down }, props, state] = createSpinbutton({
 		values: [1, 2, 3]
-	});
-
-	createEffect(() => {
-		console.log(props);
 	});
 
 	return (
 		<div {...props}>
 			<button use:up>up</button>
+			<div>{state.value}</div>
 			<button use:down>down</button>
 		</div>
 	);
--- packages/solid-apg/src/lib/spinbutton/spinbutton.ts
+++ packages/solid-apg/src/lib/spinbutton/spinbutton.ts
@@ -1,4 +1,4 @@
-import { createSignal } from "solid-js";
+import { JSX, createSignal, onCleanup } from "solid-js";
 import { combineProps } from "@solid-primitives/props";
 import type { SpinButtonArguments } from "./types";
 
@@ -8,33 +8,65 @@ import type { SpinButtonArguments } from "./types";
  * @link https://www.w3.org/WAI/ARIA/apg/patterns/spinbutton
  */
 export function createSpinbutton<T>(args: SpinButtonArguments<T>) {
-	const [role, setRole] = createSignal("spinbutton");
+	const [selectedIndex, setSelectedIndex] = createSignal(0);
+
+	const value = () => args.values[selectedIndex()];
+	const next = () => setSelectedIndex((selectedIndex() + 1) % args.values.length);
+	const prev = () => setSelectedIndex((selectedIndex() - 1 + args.values.length) % args.values.length);
+
+	const handleKeydown = (event: KeyboardEvent) => {
+		switch (event.key) {
+			case "ArrowUp":
+				next();
+				break;
+			case "Home":
+				setSelectedIndex(0);
+				break;
+			case "End":
+				setSelectedIndex(args.values.length - 1);
+				break;
+			case "ArrowDown":
+				prev();
+				break;
+		}
+	};
 
 	const up = (node: HTMLElement) => {
-		node.addEventListener("click", () => {
-			setRole("spinbutton2");
+		node.addEventListener("click", next);
+
+		onCleanup(() => {
+			node.removeEventListener("click", next);
 		});
 	};
 
 	const down = (node: HTMLElement) => {
-		node.addEventListener("mousedown", () => {
-			setRole("spinbutton3");
+		node.addEventListener("click", prev);
+
+		onCleanup(() => {
+			node.removeEventListener("click", prev);
 		});
 	};
 
-	const baseProps = {
-		get role() {
-			return role();
-		}
+	const baseProps: JSX.HTMLAttributes<HTMLElement> = {
+		role: "spinbutton",
+		tabindex: "1",
+		onKeyDown: handleKeydown
 	};
 
 	const props = combineProps(baseProps);
+
+	const state = combineProps({
+		get value() {
+			return value();
+		}
+	});
 
 	return [
 		{
 			up,
 			down
 		},
-		props
-	];
+		props,
+		state
+	] as const;
 }
