5c339e6eac4c2ceef36c23a97fa5acfbbb23b2d0
Pavel Sušický <me@frontendista.cz>
2024-03-27 16:06:20 +0100

    chore: add test coverage reporter

--- packages/solid-apg/vitest.config.ts
+++ packages/solid-apg/vitest.config.ts
@@ -6,6 +6,29 @@ export default defineConfig({
 	test: {
 		environment: "happy-dom",
 		globals: true,
-		testTransformMode: { web: ["/.[jt]sx?$/"] }
+		testTransformMode: { web: ["/.[jt]sx?$/"] },
+		coverage: {
+			provider: "v8",
+			exclude: [
+				"coverage/**",
+				"dist/**",
+				"**/[.]**",
+				"packages/*/test?(s)/**",
+				"**/*.d.ts",
+				"**/virtual:*",
+				"**/__x00__*",
+				"**/\x00*",
+				"cypress/**",
+				"test?(s)/**",
+				"test?(-*).?(c|m)[jt]s?(x)",
+				"**/*{.,-}{test,spec}.?(c|m)[jt]s?(x)",
+				"**/__tests__/**",
+				"**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
+				"**/vitest.{workspace,projects}.[jt]s?(on)",
+				"**/.{eslint,mocha,prettier}rc.{?(c|m)js,yml}",
+				"**/index.ts",
+				"typedoc.config.cjs"
+			]
+		}
 	}
 });
